5387346128e6a4ff8463c4d0fa1a37ac52b2cbc7
yj1120c <70882360+yj1120c@users.noreply.github.com>
2020-10-29 15:47:38 +0900

    03

--- main.c
+++ main.c
@@ -7,24 +7,21 @@
 int main(int argc, char *argv[]) {
 	
 	//array declaration
-	int i, average;
-	int sum;
+	int i;
 	int grade[SIZE];
+	int score[SIZE];
 	
-	printf("input %i scores\n", SIZE);
-	
-	sum = 0;
 	for (i=0;i<SIZE;i++)
 	{
-		scanf("%d", &grade[i]);
-		sum += grade[i]; 
+		grade[i] = rand()%100+1;
 	}
 	
+	//move to score
 	for(i=0;i<SIZE;i++)
-	    printf("grade[%d] = %d\n", i, grade[i]);
+	    score[i] = grade[i];
 	
-	average = sum/SIZE;
-	printf("average : %i\n", average);
+	for(i=0;i<SIZE;i++)
+	    printf("score[%d] %i (%i)\n", i, score[i], grade[i]);
 	
 	return 0;
 }
